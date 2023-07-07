3b144785c717f5686f7316d36f0a7d9a96122f26
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-07-07 22:01:10 +0000

    excuse generator

---
+++ src/script.js
@@ -0,0 +1,30 @@
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+];
+
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
+
+window.onload = function() {
+  let excuseElement = document.getElementById("excuse");
+  let generateBtn = document.getElementById("generateBtn");
+
+  generateBtn.addEventListener("click", function() {
+    excuseElement.innerHTML = generateExcuse();
+  });
+
+  excuseElement.innerHTML = generateExcuse();
+};
